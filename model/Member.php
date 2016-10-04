e4687be3f91d96a9ee930cef7dcb8621c5df38a2
Pepyn Swagemakers <ps222nq@student.lnu.se>
2016-10-04 11:55:28 +0200

    implementing Member class

--- model/Member.php
+++ model/Member.php
@@ -1,9 +1,40 @@
 <?php
 class Member {
 
-    private name;
-    private personalNumber;
-    private id;
+    private $name;
+    private $personalNumber;
+    private $id;
 
+    public function getName() {
+        return  $name;
+    }
+
+    public function setName($argName) {
+        if(is_string($argName) && strlen($argName) > 0 && strlen($argName) < 50){
+            $name = $argName;
+            return "Name set to " . $argName;
+        } else {
+            return "Name must be a string between 0 and 50 characters";
+        }
+    }
+
+    public function getPersonalNumber() {
+        return $personalNumber;
+    }
+
+    public function setPersonalNumber($argPnr) {
+    //TODO: validation of PNR
+    }
+
+    public function getId() {
+        return  $id;
+    }
+
+    public function setId($argId) {
+        //TODO: more extensive validation
+        if(is_numeric($argId)){
+            $id = $argId;
+        }
+    }
 }
 ?>
