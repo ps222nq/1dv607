b8a31d935c02b56673021088d7a8dc099d1b06cc
Pepyn Swagemakers <ps222nq@student.lnu.se>
2016-10-05 21:23:28 +0200

    start compact list methods

--- view/MemberView.php
+++ view/MemberView.php
@@ -8,4 +8,19 @@ class MemberView {
     public function renderMemberDetailsHTML(\model\Member $member){
         return '<ul><li>' . $member->getId(). ' ' .$member->getName() .' '. $member->getNumberOfAssets() . '</li></ul>';
     }
+
+    public function renderCompactList($data) {
+        $res = "<table>";
+
+        foreach($data as $d){
+            $res .= "<tr>";
+            $res .= "<td>" . $d["name"]. "</td><td>" . $d["assets"] . "</td>";
+            $res .= "</tr>";
+        }
+
+        $res .= "</table>";
+
+        return $res;
+    }
+
 }
